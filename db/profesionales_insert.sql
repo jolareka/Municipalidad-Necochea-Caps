e740dbabcbf882cc3ee85ca672017f475dc893d8
yo <ferreironisiilucila@gmail.com>
2025-07-24 16:56:53 -0300

    uwuwuwuwu
    uwuwuwuwuwu

---
+++ db/profesionales_insert.sql
@@ -0,0 +1,25 @@
+-- PROFESIONALES --
+
+-- caps flores --
+INSERT INTO `caps` (`id_caps`, `nombre`, `descripcion`, `coordenadas`, `horario`, `imagen`, `telefono`, `Campaña`) 
+VALUES (NULL, 'prueba', 'prueba', '-38.57836169346202, -58.72164468423741', '8:00', '', '1111', '3'); 
+
+-- caps flores --
+INSERT INTO `caps` (`id_caps`, `nombre`, `descripcion`, `coordenadas`, `horario`, `imagen`, `telefono`, `Campaña`) 
+VALUES (NULL, 'prueba', 'prueba', '-38.57836169346202, -58.72164468423741', '8:00', '', '1111', '3');
+
+-- caps flores --
+INSERT INTO `caps` (`id_caps`, `nombre`, `descripcion`, `coordenadas`, `horario`, `imagen`, `telefono`, `Campaña`) 
+VALUES (NULL, 'prueba', 'prueba', '-38.57836169346202, -58.72164468423741', '8:00', '', '1111', '3');
+
+-- caps flores --
+INSERT INTO `caps` (`id_caps`, `nombre`, `descripcion`, `coordenadas`, `horario`, `imagen`, `telefono`, `Campaña`) 
+VALUES (NULL, 'prueba', 'prueba', '-38.57836169346202, -58.72164468423741', '8:00', '', '1111', '3');
+
+-- caps flores --
+INSERT INTO `caps` (`id_caps`, `nombre`, `descripcion`, `coordenadas`, `horario`, `imagen`, `telefono`, `Campaña`) 
+VALUES (NULL, 'prueba', 'prueba', '-38.57836169346202, -58.72164468423741', '8:00', '', '1111', '3');
+
+-- caps flores --
+INSERT INTO `caps` (`id_caps`, `nombre`, `descripcion`, `coordenadas`, `horario`, `imagen`, `telefono`, `Campaña`) 
+VALUES (NULL, 'prueba', 'prueba', '-38.57836169346202, -58.72164468423741', '8:00', '', '1111', '3');
