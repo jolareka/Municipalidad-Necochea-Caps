e2afe19da132b2b392b262f794f53f382a7e47ba
yo <ferreironisiilucila@gmail.com>
2025-08-08 13:27:18 -0300

    Update profesionales_insert.sql
    o

--- db/profesionales_insert.sql
+++ db/profesionales_insert.sql
@@ -1,4 +1,31 @@
 -- PROFESIONALES --
 
 -- caps fucile --
-INSERT INTO `profesionales`(`id_profesionales`, `nombre`, `apellido`) VALUES ('','','')
+INSERT INTO `profesionales`(`id_profesionales`, `nombre`, `apellido`) VALUES (NULL,'Juan Jose','Segura')
+INSERT INTO `profesionales`(`id_profesionales`, `nombre`, `apellido`) VALUES (NULL,'Silvia','Coleclia')
+INSERT INTO `profesionales`(`id_profesionales`, `nombre`, `apellido`) VALUES (NULL,'Fadael','Hendrieksen')
+INSERT INTO `profesionales`(`id_profesionales`, `nombre`, `apellido`) VALUES (NULL,'Claudia','Rodriguez')
+INSERT INTO `profesionales`(`id_profesionales`, `nombre`, `apellido`) VALUES (NULL,'Alejandrina','Vasol')
+INSERT INTO `profesionales`(`id_profesionales`, `nombre`, `apellido`) VALUES (NULL,'Sonia','Amaya')
+INSERT INTO `profesionales`(`id_profesionales`, `nombre`, `apellido`) VALUES (NULL,'Susana','Boudolof')
+INSERT INTO `profesionales`(`id_profesionales`, `nombre`, `apellido`) VALUES (NULL,'Juliana','Podio')
+INSERT INTO `profesionales`(`id_profesionales`, `nombre`, `apellido`) VALUES (NULL,'Mario','De Marizel')
+INSERT INTO `profesionales`(`id_profesionales`, `nombre`, `apellido`) VALUES (NULL,'Liliana','Eguibar')
+INSERT INTO `profesionales`(`id_profesionales`, `nombre`, `apellido`) VALUES (NULL,'Eugenia','Amado')
+
+--- caps sur ---
+INSERT INTO `profesionales`(`id_profesionales`, `nombre`, `apellido`) VALUES (NULL,'Sofia','Molina')
+INSERT INTO `profesionales`(`id_profesionales`, `nombre`, `apellido`) VALUES (NULL,'Myriam','Aranda')
+INSERT INTO `profesionales`(`id_profesionales`, `nombre`, `apellido`) VALUES (NULL,'Flavia','Aguero')
+INSERT INTO `profesionales`(`id_profesionales`, `nombre`, `apellido`) VALUES (NULL,'Agustina','Benitez')
+INSERT INTO `profesionales`(`id_profesionales`, `nombre`, `apellido`) VALUES (NULL,'Maria Jose','Bamonde')
+INSERT INTO `profesionales`(`id_profesionales`, `nombre`, `apellido`) VALUES (NULL,'Carolina','Marchal')
+INSERT INTO `profesionales`(`id_profesionales`, `nombre`, `apellido`) VALUES (NULL,'Silvina','Sorense')
+--- INSERT INTO `profesionales`(`id_profesionales`, `nombre`, `apellido`) VALUES (NULL,'Juliana','Podio') --- repetida en fucile ---
+INSERT INTO `profesionales`(`id_profesionales`, `nombre`, `apellido`) VALUES (NULL,'Nancy','Andueza')
+INSERT INTO `profesionales`(`id_profesionales`, `nombre`, `apellido`) VALUES (NULL,'Carolina','Velazquez')
+INSERT INTO `profesionales`(`id_profesionales`, `nombre`, `apellido`) VALUES (NULL,'Daniela','Luna')
+INSERT INTO `profesionales`(`id_profesionales`, `nombre`, `apellido`) VALUES (NULL,'Paula','Gaitan')
+
+--- caps 6 esquinas ---
+
